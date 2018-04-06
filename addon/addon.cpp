49d047618d839cf9c32b68b2d511c1c6d71a5ded
Bass Jansson <bassjansson@gmail.com>
2018-04-06 21:03:53 +0200

    TG-23 #delivered Adds successful native node addon test.

---
+++ addon/addon.cpp
@@ -0,0 +1,27 @@
+// hello.cc
+#include <node.h>
+
+namespace addon
+{
+
+using v8::FunctionCallbackInfo;
+using v8::Isolate;
+using v8::Local;
+using v8::Object;
+using v8::String;
+using v8::Value;
+
+void printHelloWorld(const FunctionCallbackInfo<Value> &args)
+{
+    Isolate *isolate = args.GetIsolate();
+    args.GetReturnValue().Set(String::NewFromUtf8(isolate, "Hello World!"));
+}
+
+void initializeModule(Local<Object> exports)
+{
+    NODE_SET_METHOD(exports, "printHelloWorld", printHelloWorld);
+}
+
+NODE_MODULE(NODE_GYP_MODULE_NAME, initializeModule)
+
+} // namespace addon
---
+++ app/app.js
@@ -0,0 +1,5 @@
+// Require native addon
+const addon = require('../addon');
+
+// Prints 'Hello World!'
+console.log(addon.printHelloWorld());
