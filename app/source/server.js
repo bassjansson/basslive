553fc7cd1c27ec826507abd1668fa691329427e2
Bass Jansson <bassjansson@gmail.com>
2018-04-10 13:54:10 +0200

    Improves server.

--- app/source/server.js
+++ app/source/server.js
@@ -3,23 +3,22 @@
 //====================================//
 
 // Require and create the server
-const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
-const port = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const port = process.env.PORT || 3000
 
 // Serve the public directory
-app.use(express.static('source/public'));
+app.use(express.static('source/public'))
 
 // Start listening on the server port
 server.listen(port, () =>
-{
-    console.log('[Server] Listening on port: ', port);
-});
+    console.log('[Server] Listening on port: ',
+        server.address().port))
 
 
 //===================================//
 //========== Export Module ==========//
 //===================================//
 
-module.exports = server;
+module.exports = server
