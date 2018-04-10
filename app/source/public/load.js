adc6e8e711590ec9527454a8209e1cdde28386fc
Bass Jansson <bassjansson@gmail.com>
2018-04-10 15:54:44 +0200

    Adds a load script so client and host use the same html page.

---
+++ app/source/public/load.js
@@ -0,0 +1,20 @@
+/*
+ *  Loads specific host and/or client scripts.
+ */
+
+if (typeof require === 'function')
+{
+    require('../host')
+}
+else
+{
+    function loadScript(src, onload = null)
+    {
+        let script = document.createElement('script')
+        script.src = src
+        script.onload = onload
+        document.head.appendChild(script)
+    }
+
+    loadScript('/socket.io/socket.io.js', () => loadScript('client.js'))
+}
