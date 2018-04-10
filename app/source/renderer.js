f68bde578a521b6ab1de220bbb0cd801af6579a2
Bass Jansson <bassjansson@gmail.com>
2018-04-10 13:23:19 +0200

    Adds an express server to the app.

--- app/source/renderer.js
+++ app/source/renderer.js
@@ -2,6 +2,9 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
+// Require the server
+const server = require('./server')
+
 // Require basslive engine
 const engine = require('basslive-engine')
 
---
+++ app/source/server.js
@@ -0,0 +1,25 @@
+//====================================//
+//========== Express Server ==========//
+//====================================//
+
+// Require and create the server
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const port = process.env.PORT || 3000;
+
+// Serve the public directory
+app.use(express.static('source/public'));
+
+// Start listening on the server port
+server.listen(port, () =>
+{
+    console.log('[Server] Listening on port: ', port);
+});
+
+
+//===================================//
+//========== Export Module ==========//
+//===================================//
+
+module.exports = server;
